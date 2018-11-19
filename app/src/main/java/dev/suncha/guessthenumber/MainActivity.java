2aaa08b9def87add0e14897cead652435281a3c5
sunnyshrestha <dev.suncha@gmail.com>
2018-11-19 22:28:40 +0545

    Random number generated successfully

--- app/src/main/java/dev/suncha/guessthenumber/MainActivity.java
+++ app/src/main/java/dev/suncha/guessthenumber/MainActivity.java
@@ -2,15 +2,19 @@ package dev.suncha.guessthenumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     EditText lowerEditText, upperEditText;
     Button generatorButton;
     int lowerNumber, higherNumber, randomNumber;    //Defining the variables
+    private  static int OPTIMUM_RANGE=10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +26,51 @@ public class MainActivity extends AppCompatActivity {
         upperEditText = findViewById(R.id.editTextHigher);
         generatorButton = findViewById(R.id.button);
 
-        //Getting the inputs
-        lowerNumber = Integer.parseInt(lowerEditText.getText().toString());
-        higherNumber = Integer.parseInt(upperEditText.getText().toString());
-
-
         //This code gets triggered when the Generate Random Number button is clicked
         generatorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Check for null entries
-                do{
-                    //If the fields are not empty, do the following:
-                    //Check that input for higher number is greater than input for lower number before generating random number
-                    //Make sure that the range is good enough for the game to be interesting
-                    //Once the two things above are accomplished, generate the random number when the button is clicked
-                } while (checkForNullEntries(lowerEditText, upperEditText));
+                //Convert EditText entries to integer avoiding number format exception
+                try{
+                    lowerNumber = Integer.parseInt(String.valueOf(lowerEditText.getText()));
+                }catch (NumberFormatException nfe){
+                    lowerNumber = 0;
+                }
 
+                try{
+                    higherNumber = Integer.parseInt(String.valueOf(upperEditText.getText()));
+                }catch (NumberFormatException nfe){
+                    higherNumber=0;
+                }
+
+                //Check for null entries
+                if(checkForNullEntries(lowerEditText,upperEditText)){
+                    //If the fields are not empty, do the following:
+                    //Make sure that the range is good enough for the game to be interesting
+                    if(rangeOfNumbers(lowerNumber,higherNumber)>=OPTIMUM_RANGE){
+                        //Generate random number
+                        randomNumber = generateRandomNumber(lowerNumber,higherNumber);
+                        Log.d("Random number = ", Integer.toString(randomNumber));
+                    }else if(lowerNumber == higherNumber){
+                        Toast.makeText(getApplicationContext(),R.string.cannotBeEqual,Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(),R.string.notInteresting,Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
+    }
+
+    private int generateRandomNumber(int lowerNumber, int higherNumber) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((higherNumber-lowerNumber)+1)+lowerNumber;
+        return randomNum;
+    }
+
+    //Calculate the absolute value of range
+    private int rangeOfNumbers(int lowerNumber, int higherNumber) {
+        return (Math.abs(lowerNumber-higherNumber));
     }
 
     private boolean checkForNullEntries(EditText lowerEditText, EditText upperEditText) {
@@ -50,6 +79,5 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return  true;
-
     }
 }
